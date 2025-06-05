4c85d1d21c56867bcb7ee856e0ba5aea952d0a72
JMTamayo <jmtamayog23@gmail.com>
2025-06-04 22:04:26 -0500

    Implement config module to handle project configuration parameters

---
+++ src/config/conf.cpp
@@ -0,0 +1,9 @@
+#include "conf.h"
+
+namespace config {
+
+const unsigned long DEFAULT_BAUD_RATE = 115200;
+
+const unsigned long BUILTIN_LED_PIN = 13;
+
+} // namespace config
---
+++ src/config/conf.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace config {
+
+// Serial Configuration parameters:
+extern const unsigned long DEFAULT_BAUD_RATE; // Default baud rate for serial
+                                              // communication
+
+// I/O Pin Labeling:
+extern const unsigned long
+    BUILTIN_LED_PIN; // Built-in LED pin for microcontroller
+
+} // namespace config
