2ce5d7f517655b5b7a5af23e8594e928decc8381
JMTamayo <jmtamayog23@gmail.com>
2025-06-07 10:31:36 -0500

    Change pin value types from long to int. Implement tank parameters to implement the level controller

--- src/config/conf.cpp
+++ src/config/conf.cpp
@@ -4,10 +4,20 @@ namespace config {
 
 const unsigned long DEFAULT_BAUD_RATE = 115200;
 
-const unsigned long BUILTIN_LED_PIN = 13;
+const unsigned int BUILTIN_LED_PIN = 13;
 
-const unsigned long HC_SR04_ECHO_PIN = 3;
+const unsigned int HC_SR04_ECHO_PIN = 3;
 
-const unsigned long HC_SR04_TRIGGER_PIN = 2;
+const unsigned int HC_SR04_TRIGGER_PIN = 2;
+
+const unsigned int INLET_VALVE_PIN = 4;
+
+const float TANK_HEIGHT_MM = 100.0;
+
+const float MIN_LEVEL_MM = 20.0;
+
+const float MAX_LEVEL_MM = 80.0;
+
+const int MIN_PUMP_DURATION_MS = 3000;
 
 } // namespace config
